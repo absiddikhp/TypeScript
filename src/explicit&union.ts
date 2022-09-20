e5d70c4f23e1ba7957e34fdb59398efa7dc08ed2
absiddikhp <absiddikhp@gmail.com>
2022-09-21 02:45:49 +0600

    explicit and unioun type in TypeScript

---
+++ src/explicit&union.ts
@@ -0,0 +1,32 @@
+// let a: string
+// let b : number
+// a = "siddik"
+// b=3
+// console.table(a,b);
+
+
+
+
+// // single 
+// let singleArray: string | number;
+
+// // array
+// let newArray: (string | boolean)[] = []
+// newArray.push(true, "Siddik")
+// console.log(newArray);
+
+// singleArray = ("Siddik")
+// console.log(singleArray);
+
+
+// // object
+// let newObject: {
+//     name: string,
+//     age: number,
+//     isAdult: boolean,
+// };
+// newObject = {
+//     name: "siddik",
+//     age: 30,
+//     isAdult: true
+// }
