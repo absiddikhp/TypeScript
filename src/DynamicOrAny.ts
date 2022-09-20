ad5115c3723a848fdba370f32105176bc8c3978d
absiddikhp <absiddikhp@gmail.com>
2022-09-21 02:54:22 +0600

    Any type of data In TypeScript

---
+++ src/DynamicOrAny.ts
@@ -0,0 +1,20 @@
+// // any type of typeScript 
+// // never use this any type. because if you are use this any type ultimatly you use js. for that you will lose type script flavor.
+// let a :any;
+// a=5
+// a="siddik"
+
+
+
+// let a: any[] = []
+// a.push("siddik")
+
+// let b: {
+//     name: any,
+//     age: any
+// }
+// b = {
+//     name: "Siddik",
+//     age: 30
+// }
+// console.log(b);
